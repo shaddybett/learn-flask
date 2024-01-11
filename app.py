2216f1dc87162d9e0e7cce5ab33f9cccda721211
Shaddy <shadybett540@gmail.com>
2024-01-11 22:51:18 +0300

    done

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from models import Book,Author
+from models import db, Book, Author
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///learn.db'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+# Create the database tables
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
@@ -12,13 +17,13 @@ def home():
 
 @app.route('/books')
 def books():
-    books_list= Book.query_all()
+    books_list = Book.query.all()
     return render_template('books.html', books=books_list)
 
 @app.route('/authors')
 def authors():
-    authors_list = Author.query_all()
-    return render_template('authors.html',authors=authors_list)   
+    authors_list = Author.query.all()
+    return render_template('authors.html', authors=authors_list)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
