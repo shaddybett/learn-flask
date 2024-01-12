590586baad487c3a0d4cd9f52d6d2ac0c7829369
Shaddy <shadybett540@gmail.com>
2024-01-12 13:37:19 +0300

    done

--- app.py
+++ app.py
@@ -1,31 +1,59 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
+# from flask_sqlalchemy import SQLAlchemy
+# from models import db, Book, Author
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///learn.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db.init_app(app)
+
+# # Create the database tables
+# # with app.app_context():
+# #     db.create_all()
+
+# @app.route('/')
+# def home():
+#     return 'Welcome home!'
+
+# @app.route('/books')
+# def books():
+#     books_list = Book.query.all()
+#     return render_template('books.html', books=books_list)
+
+# @app.route('/authors')
+# def authors():
+#     authors_list = Author.query.all()
+#     return render_template('authors.html', authors=authors_list)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
-from models import db, Book, Author
+from models import db,Book,Author
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///learn.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///best.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-# Create the database tables
-# with app.app_context():
-#     db.create_all()
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
-    return 'Welcome home!'
+    return 'HEllo and welcome'
 
 @app.route('/books')
 def books():
     books_list = Book.query.all()
-    return render_template('books.html', books=books_list)
+    return render_template('books.html',books = books_list)
 
 @app.route('/authors')
 def authors():
     authors_list = Author.query.all()
-    return render_template('authors.html', authors=authors_list)
+    return render_template('authors.html',authors = authors_list)
 
-if __name__ == '__main__':
+if __name__=='__main__':
     app.run(debug=True)
-
-
